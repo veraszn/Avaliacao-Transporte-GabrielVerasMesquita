610cd5401748deeb691bd39cefe6aea7b43ad177
veraszn <gabriel.veras.mes@gmail.com>
2026-03-07 13:41:24 -0300

    Alteracao na lista de rotas

---
+++ app.py
@@ -0,0 +1,9 @@
+
+print("Rotas:") 
+print("Rota 1 - Umirim")
+print("Rota 2 - Croatá")
+print("Rota 3 - São Luis do Curu")
+print("Rota 4 - Itapajé")
+print("Rota 5 - Uruburetama")
+print("Rota 6 - Itapipoca")
+
