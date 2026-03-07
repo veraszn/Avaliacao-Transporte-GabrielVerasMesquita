a4de7ade0121bce5144414e60175090748c4cec2
veraszn <gabriel.veras.mes@gmail.com>
2026-03-07 13:54:34 -0300

    Horarios Criados

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
+def Rotas():
+    print("\nLista de Rotas:") 
+    print("Rota 1 - Umirim")
+    print("Rota 2 - Croatá")
+    print("Rota 3 - São Luis do Curu")
+    print("Rota 4 - Itapajé")
+    print("Rota 5 - Uruburetama")
+    print("Rota 6 - Itapipoca")
 
-print("Rotas:") 
-print("Rota 1 - Umirim")
-print("Rota 2 - Croatá")
-print("Rota 3 - São Luis do Curu")
-print("Rota 4 - Itapajé")
-print("Rota 5 - Uruburetama")
-print("Rota 6 - Itapipoca")
-
+def turno(hora):
+    if hora >= 6 and hora < 8:
+        return "Manhã"
+    
+    elif hora >= 12 and hora < 14:
+        return "Tarde"
+    
+    elif hora >= 16 and hora < 18:
+        return "Final da Tarde"
+    
+    else:
+        return "Não temos transporte neste horário."
